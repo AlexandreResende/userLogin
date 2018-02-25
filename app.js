935c041a9f7fbca6a1fa66f7d7e2f83055acc432
Alexandre Resende <aar6947@gmail.com>
2018-02-24 22:58:17 -0300

    chore: Adjust app.js indentation and add joi for validation

--- app.js
+++ app.js
@@ -25,46 +25,46 @@ const app = express();
 
 // view engine setup
 app
-.set('views', path.join(__dirname, 'views'))
-.set('view engine', 'jade');
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app
-.use(logger('dev'))
-.use(bodyParser.json())
-.use(bodyParser.urlencoded({ extended: false }))
-.use(cookieParser())
-.use(express.static(path.join(__dirname, 'public')))
-.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}))
-.use(passport.initialize())
-.use(passport.session())
-.use(expressValidator())
-.use(require('connect-flash')())
-.use(function (req, res, next) {
-  res.locals.messages = require('express-messages')(req, res);
-  next();
-})
-.use('/', index)
-.use('/users', users)
-.use(function(req, res, next) {// catch 404 and forward to error handler
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-})
-.use(function(err, req, res, next) {// error handler
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  .use(logger('dev'))
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .use(cookieParser())
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(session({
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+  }))
+  .use(passport.initialize())
+  .use(passport.session())
+  .use(expressValidator())
+  .use(require('connect-flash')())
+  .use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+  })
+  .use('/', index)
+  .use('/users', users)
+  .use(function(req, res, next) {// catch 404 and forward to error handler
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  })
+  .use(function(err, req, res, next) {// error handler
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
 
 app.get('*', (req, res, next) => {
   res.locals.user = req.user || null;
